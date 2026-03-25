89551e5da526a0e78b7ac11be3ce72c29f0b8859
SearedBacon <2260910@flourbluffschools.org>
2026-03-25 13:08:24 -0500

    Started creating forest and added a lake for water to be collected

---
+++ wizard-doctors-main/scripts/bookshelf.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_recipe_pressed() -> void:
+	Globals.rScreen=true
+
+
+func _on_index_pressed() -> void:
+	Globals.iScreen=true
---
+++ wizard-doctors-main/scripts/globals.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var ipage:=0
+var rpage:=0
+var ingredientpage:=0
+var IngredientScreen=false
+var rScreen=false
+var iScreen=false
+var treeroot:=0
