074b58ae419c54051176d39ee727e0ed251c6310
SearedBacon <2260910@flourbluffschools.org>
2026-03-30 12:40:22 -0500

    added an enemy and more ingredients to collect and started working on how the fights looks and works

---
+++ wizard-doctors-main/scripts/boar.gd
@@ -0,0 +1,38 @@
+extends Area2D
+class_name Boar
+
+@onready var boar: Boar = $"."
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Globals.run==true:
+		if Globals.left==true:
+			boar.global_position.x-=1
+			animated_sprite.play("Left")
+			await get_tree().create_timer(2).timeout
+			Globals.left=false
+			Globals.up=true
+			
+		if Globals.up==true:
+			boar.global_position.y-=0.4
+			animated_sprite.play("Up")
+			await get_tree().create_timer(2).timeout
+			Globals.up=false
+			Globals.right=true
+			
+		if Globals.right==true:
+			boar.global_position.x+=0.33
+			animated_sprite.play("Right")
+			await get_tree().create_timer(2).timeout
+			Globals.right=false
+			Globals.down=true
+			
+		if Globals.down==true:
+			boar.global_position.y+=0.2
+			animated_sprite.play("Down")
+			await get_tree().create_timer(2).timeout
+			Globals.down=false
+			Globals.left=true
+	
