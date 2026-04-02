f2d1fcfecdaf0a77c66a210c73389d811d187857
SearedBacon <2260910@flourbluffschools.org>
2026-04-02 12:38:22 -0500

    Made the fight exitable and I started on creating the village

--- wizard-doctors-main/scripts/fight.gd
+++ wizard-doctors-main/scripts/fight.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 @onready var attacks: Node2D = $Attacks
-
 @onready var player: AnimatedSprite2D = $player
 
 func _physics_process(delta: float) -> void:
@@ -13,3 +12,8 @@ func _physics_process(delta: float) -> void:
 
 func _on_attack_pressed() -> void:
 	attacks.visible=true
+
+
+func _on_run_pressed() -> void:
+	Globals.fightb=false
+	get_tree().change_scene_to_file("res://scenes/forest.tscn")
