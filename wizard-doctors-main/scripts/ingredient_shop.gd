f23af3d6bb23f7310ee29a56374711a3465a446e
SearedBacon <2260910@flourbluffschools.org>
2026-05-15 12:38:28 -0500

    finally got the spell shop to not destroy itself and added some code

--- wizard-doctors-main/scripts/ingredient_shop.gd
+++ wizard-doctors-main/scripts/ingredient_shop.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
-
 func _on_tusk_buy_pressed() -> void:
-	Globals.money-=10
+	if Globals.money>=10:
+		Globals.money-=10
+		BoarGlobals.tusk+=1
 
 func _on_hoof_buy_pressed() -> void:
-	Globals.money-=10
+	if Globals.money>=10:
+		Globals.money-=10
+		BoarGlobals.hoof+=1
 
 func _on_scale_buy_pressed() -> void:
-	Globals.money-=10
+	if Globals.money>=10:
+		Globals.money-=10
+		FishGlobals.scale+=1
 
 func _on_bone_buy_pressed() -> void:
-	Globals.money-=10
+	if Globals.money>=10:
+		Globals.money-=10
+		FishGlobals.bone+=1
---
+++ wizard-doctors-main/scripts/spell_shop.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var yes_quake:=false
+var yes_spray:=false
+var yes_hole:=false
+var yes_dance:=false
+
+func _on_quake_buy_pressed() -> void:
+	if Globals.money>=100 and yes_quake==false:
+		Globals.money-=100
+		yes_quake=true
+
+func _on_spray_buy_pressed() -> void:
+	if Globals.money>=250 and yes_spray==false:
+		Globals.money-=250
+		yes_spray=true
+
+func _on_hole_buy_pressed() -> void:
+	if Globals.money>=500 and yes_hole==false:
+		Globals.money-=500
+		yes_hole=true
+
+func _on_dance_buy_pressed() -> void:
+	if Globals.money>=1000 and yes_dance==false:
+		Globals.money-=1000
+		yes_dance=true
